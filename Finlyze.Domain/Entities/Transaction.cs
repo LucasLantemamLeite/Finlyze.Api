d3cb37a2d13cf1c09c56ae3bb47b21d1fc476e14
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-06-25 04:06:23 -0300

    feat: entidade transaction criada, Closes #9

---
+++ Finlyze.Domain/Entities/Transaction.cs
@@ -0,0 +1,23 @@
+using Finlyze.Domain.ValueObject.TransactionObjects;
+
+namespace Finlyze.Domain.Entity;
+
+public class Transaction : Entity
+{
+    public Title Title { get; private set; }
+    public Description? Description { get; private set; }
+    public Amount Amount { get; private set; }
+    public TypeTransaction TypeTransaction { get; private set; }
+    public CreateAt CreateAt { get; private set; }
+    public UpdateAt UpdateAt { get; private set; }
+
+    public Transaction(string title, string? description, decimal? amount, int type, DateTime create, DateTime update)
+    {
+        Title = new Title(title);
+        Description = new Description(description);
+        Amount = new Amount(amount);
+        TypeTransaction = new TypeTransaction(type);
+        CreateAt = new CreateAt(create);
+        UpdateAt = new UpdateAt(update);
+    }
+}
