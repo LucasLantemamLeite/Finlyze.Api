93bdf92076c14cac38b18d89786600adda0a83b0
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-06-25 04:55:29 -0300

    feat: adicionado a validação no Amount para não aceitar valores igual a zero

--- Finlyze.Domain/ValueObjects/TransactionObjects/AmountObject.cs
+++ Finlyze.Domain/ValueObjects/TransactionObjects/AmountObject.cs
@@ -1,11 +1,14 @@
+using Finlyze.Domain.ValueObject.Validation;
+
 namespace Finlyze.Domain.ValueObject.TransactionObjects;
 
 public class Amount : ValueObject
 {
     public decimal Value { get; set; }
 
-    public Amount(decimal? amount)
+    public Amount(decimal amount)
     {
-        Value = amount ?? 0;
+        DomainException.ThrowIfFalse(amount != 0, "Amount não pode ser igual a 0.");
+        Value = amount;
     }
 }
