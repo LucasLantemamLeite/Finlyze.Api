38d5739c6172c669fcb33995871f4a96ea592ac1
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-06-25 19:45:39 -0300

    feat: criado o enum ELog para enumerar os tipos de log no banco

---
+++ Finlyze.Domain/Enums/ELog.cs
@@ -0,0 +1,15 @@
+namespace Finlyze.Domain.ValueObject.Enums;
+
+public enum ELog
+{
+    Info = 1,
+    Warning = 2,
+    Error = 3,
+    Audit = 4,
+    Security = 5,
+    Transaction = 6,
+    Category = 7,
+    System = 8,
+    ExternalService = 9,
+    Validation = 10
+}
