bedbe34cde95ca2de18ababf8e9f5e63f274c5da
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-06-26 11:31:34 -0300

    refactor: ajuste no nome da propriedade Description para não causar confusão com outras entidades

--- Finlyze.Domain/Entities/Transaction.cs
+++ Finlyze.Domain/Entities/Transaction.cs
@@ -6,7 +6,7 @@ namespace Finlyze.Domain.Entity;
 public class Transaction : Entity
 {
     public TransactionTitle TransactionTitle { get; private set; }
-    public TransactionDescription? Description { get; private set; }
+    public TransactionDescription? TransactionDescription { get; private set; }
     public Amount Amount { get; private set; }
     public TypeTransaction TypeTransaction { get; private set; }
     public TransactionCreateAt TransactionCreateAt { get; private set; }
@@ -15,7 +15,7 @@ public class Transaction : Entity
     public Transaction(string title, string? description, decimal amount, int type, DateTime create)
     {
         TransactionTitle = new TransactionTitle(title);
-        Description = new TransactionDescription(description);
+        TransactionDescription = new TransactionDescription(description);
         Amount = new Amount(amount);
         TypeTransaction = new TypeTransaction(type);
         TransactionCreateAt = new TransactionCreateAt(create);
