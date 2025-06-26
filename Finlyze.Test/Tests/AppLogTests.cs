2e208667a2c1db8557e59d050fa880e98a8c6595
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-06-26 11:32:17 -0300

    test: adicionado teste unitário da entidade AppLog, Closes #12

---
+++ Finlyze.Test/Tests/AppLogTests.cs
@@ -0,0 +1,57 @@
+using Finlyze.Domain.ValueObject.AppLogObjects;
+using Finlyze.Domain.ValueObject.Validation;
+
+namespace Finlyze.Test.Validation.AppLogTest;
+
+
+[Trait("Category", "AppLog")]
+public class AppLogTest
+{
+
+    [Theory]
+    [InlineData("Conta Criada")]
+    [InlineData("Transcation Criada")]
+    [InlineData("Conta Delete")]
+    public static void Dado_Um_Title_Válido_Adiciona_Na_Entidade(string title)
+    {
+        var ex = Record.Exception(() => new LogTitle(title));
+        Assert.Null(ex);
+    }
+
+
+    [Theory]
+    [InlineData("")]
+    [InlineData("  ")]
+    [InlineData("   ")]
+    [InlineData(null)]
+    public static void Dado_Um_Title_Vazio_Ou_Nulo_Lança_Exceção(string title)
+    {
+        var ex = Assert.Throws<DomainException>(() => new LogTitle(title));
+        Assert.NotNull(ex);
+        Assert.Equal("Title não pode ser vazio ou nulo.", ex.Message);
+    }
+
+    [Theory]
+    [InlineData(1)]
+    [InlineData(2)]
+    [InlineData(3)]
+    public static void Dado_Um_LogType_Válido_Adiciona_Na_Entidade(int type)
+    {
+        var ex = Record.Exception(() => new LogType(type));
+        Assert.Null(ex);
+    }
+
+    [Theory]
+    [InlineData(11)]
+    [InlineData(12)]
+    [InlineData(999)]
+    [InlineData(-1)]
+    public static void Dado_Um_LogType_Inválido_Lança_Exceção(int type)
+    {
+        var ex = Assert.Throws<EnumException>(() => new LogType(type));
+        Assert.NotNull(ex);
+        Assert.Equal("Enum Inválido.", ex.Message);
+    }
+
+
+}
