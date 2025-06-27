44997836f8239af8713cae59dec80883792d03e3
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-06-27 19:16:09 -0300

    criado o UserAccountDto para ser utilizados nos repository

---
+++ Finlyze.Application/DTOs/UserAccountDto.cs
@@ -0,0 +1,14 @@
+namespace Finlyze.Application.Interface.Dto;
+
+public class UserAccountDto
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string PhoneNumber { get; set; }
+    public DateOnly BirthDate { get; set; }
+    public DateTime CreateAt { get; set; }
+    public bool Active { get; set; }
+    public int Role { get; set; }
+}
