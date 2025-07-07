73271bdcb3ebb47d7a58a6df2a58eb866a9728dc
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-07-07 15:44:53 -0300

    feat: adicionado a configuração do Authentication validando Tokens recebido do FrontEnd, Closes #42

--- Finlyze.Api/Configuration/Builder/BuilderConfig.cs
+++ Finlyze.Api/Configuration/Builder/BuilderConfig.cs
@@ -1,4 +1,8 @@
+using System.Text;
+using Finlyze.Api.Services.Jwt;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Finlyze.Api.Configuration.Builder;
 
@@ -6,9 +10,22 @@ public static class BuilderConfig
 {
     public static WebApplicationBuilder RegisterConfig(this WebApplicationBuilder builder)
     {
-        // builder.Services.AddAuthentication();
-        // builder.Services.AddAuthorization(); 
+        builder.Services.AddAuthentication(x =>
+        {
+            x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(x =>
+        {
+            x.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(JwtSettings.JwtKey)),
+                ValidateIssuer = false,
+                ValidateAudience = false
+            };
+        });
 
+        builder.Services.AddAuthorization();
         builder.Services.Configure<ApiBehaviorOptions>(options => options.SuppressModelStateInvalidFilter = true);
 
         if (builder.Environment.IsDevelopment())
