6ce923d16f9c225410180bf759b7da86fac734cf
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-07-07 15:07:36 -0300

    feat: criado o método GenerateToken para gerar Token com base o usuário logado, Closes #39

---
+++ Finlyze.Api/Services/JwtTokenHandler.cs
@@ -0,0 +1,34 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Finlyze.Api.Services.Jwt;
+using Finlyze.Domain.Entity;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Finlyze.Api.Services.TokenHandler;
+
+public class JwtTokenHandler
+{
+    public static string GenerateToken(UserAccount user)
+    {
+        var handler = new JwtSecurityTokenHandler();
+        var key = Encoding.ASCII.GetBytes(JwtSettings.JwtKey);
+        var credencials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256);
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(
+            [
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.GivenName, user.Name.Value),
+                new Claim("Active", user.Active.Value.ToString()),
+                new Claim(ClaimTypes.Role, user.Role.Value.ToString())
+            ]),
+
+            SigningCredentials = credencials,
+            Expires = DateTime.UtcNow.AddHours(6)
+        };
+
+        var token = handler.CreateToken(tokenDescriptor);
+        return handler.WriteToken(token);
+    }
+}
