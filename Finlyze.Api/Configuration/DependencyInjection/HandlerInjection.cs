316213cdf1daf0ff83524cf6fc83111d1b9c5e6f
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-07-07 22:20:00 -0300

    feat: criado um extension method para separar a injeção dos handler dos services

---
+++ Finlyze.Api/Configuration/DependencyInjection/HandlerInjection.cs
@@ -0,0 +1,14 @@
+using Finlyze.Application.Abstract.Interface;
+using Finlyze.Infrastructure.Implementation.Interfaces.Handler;
+
+namespace Finlyze.Api.Configuration.DependencyInjection;
+
+public static class HandlerInjection
+{
+    public static IServiceCollection RegisterHandlers(this IServiceCollection service)
+    {
+        service.AddScoped<ICreateUserAccountHandler, CreateUserAccountHandler>();
+
+        return service;
+    }
+}
