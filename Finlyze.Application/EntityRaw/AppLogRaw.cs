545ba68841e8a983f6b5f9ed0e3f602b2f4cbbac
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-07-07 21:45:50 -0300

    chore: sem mudança aparente, provavelmente bug do editor

---
+++ Finlyze.Application/EntityRaw/AppLogRaw.cs
@@ -0,0 +1,10 @@
+namespace Finlyze.Application.Entity.Raw;
+
+public class AppLogRaw
+{
+    public Guid Id { get; set; }
+    public int LogType { get; set; }
+    public string LogTitle { get; set; }
+    public string LogDescription { get; set; }
+    public DateTime LogCreateAt { get; set; }
+}
