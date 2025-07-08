1fc66cfe00cb756d4aea640da8cc485fe23a7ea7
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-07-08 15:04:40 -0300

    feat: adicionado o construtor do command e novas propriedades

--- Finlyze.Application/Command/UserAccountCommand/UpdateUserAccountCommand.cs
+++ Finlyze.Application/Command/UserAccountCommand/UpdateUserAccountCommand.cs
@@ -3,8 +3,21 @@ namespace Finlyze.Application.Abstract.Interface.Command;
 public class UpdateUserAccountCommand
 {
     public Guid Id { get; }
+    public string ConfirmPassword { get; set; }
     public string? Name { get; }
     public string? Email { get; }
     public string? Password { get; }
     public string? PhoneNumber { get; }
+    public DateOnly? BirthDate { get; set; }
+
+    public UpdateUserAccountCommand(Guid id, string confirm_password, string name, string email, string password, string phone_number, DateOnly birth)
+    {
+        Id = id;
+        ConfirmPassword = confirm_password;
+        Name = name;
+        Email = email;
+        Password = password;
+        PhoneNumber = phone_number;
+        BirthDate = birth;
+    }
 }
