4f4accdcd1e426cd595e225b1d53f2bbf6ec8925
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-07-07 22:21:39 -0300

    refactor: movido a injeção dos handler para outra classe

--- Finlyze.Api/Configuration/DependencyInjection/ServiceInjection.cs
+++ Finlyze.Api/Configuration/DependencyInjection/ServiceInjection.cs
@@ -1,7 +1,5 @@
 using System.Data;
 using Finlyze.Api.Services.Jwt;
-using Finlyze.Application.Abstract.Interface;
-using Finlyze.Infrastructure.Implementation.Interfaces.Handler;
 using Finlyze.Migrations.Data.Context;
 using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
@@ -17,9 +15,6 @@ public static class ServiceInjection
         service.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
         service.AddScoped<IDbConnection>(sp => new SqlConnection(connectionString));
         JwtSettings.JwtKey = configuration["JwtSettings:JwtKey"] ?? throw new Exception("JwtKey não encontrada");
-
-        service.AddScoped<ICreateUserAccountHandler, CreateUserAccountHandler>();
-        service.AddScoped<ILoginUserAccountHandler, LoginUserAccountHandler>();
 
         return service;
     }
