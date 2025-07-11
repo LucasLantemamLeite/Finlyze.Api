1d1b3aa2e7ae81ad38d9afbe15ea8d61a2c0658d
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-07-11 14:41:33 -0300

    feat: criado e implementado o Handler para a criação de Transações, Closes #52

---
+++ Finlyze.Infrastructure/Handler/Transaction/CreateTransactionHandler.cs
@@ -0,0 +1,59 @@
+using Finlyze.Application.Abstract.Interface;
+using Finlyze.Application.Abstract.Interface.Command;
+using Finlyze.Application.Abstract.Interface.Handler;
+using Finlyze.Application.Abstract.Interface.Handler.Result;
+using Finlyze.Domain.Entity;
+using Finlyze.Domain.ValueObject.Enums;
+using Finlyze.Domain.ValueObject.Validation;
+
+namespace Finlyze.Infrastructure.Implementation.Interfaces.Handler;
+
+public class CreateTransactionHandler : ICreateTransactionHandler
+{
+    private readonly IUserAccountQuery _userQuery;
+    private readonly ITransactionRepository _transRepository;
+    private readonly IAppLogRepository _appRepository;
+
+    public CreateTransactionHandler(IUserAccountQuery userQuery, ITransactionRepository transReposotory, IAppLogRepository appRepository)
+    {
+        _userQuery = userQuery;
+        _transRepository = transReposotory;
+        _appRepository = appRepository;
+    }
+
+    public async Task<ResultHandler<Transaction>> Handle(CreateTransactionCommand command)
+    {
+        try
+        {
+            var existingUser = await _userQuery.GetByIdAsync(command.UserAccountId);
+
+            if (existingUser is not null)
+                return ResultHandler<Transaction>.Fail("Usuário com esse Id não encontrado.");
+
+            var transaction = new Transaction(command.TransactionTitle, command.TransactionDescription, command.Amount, command.TypeTransaction, command.TransactionCreateAt);
+            var row = await _transRepository.CreateAsync(transaction);
+
+            if (row == 0)
+            {
+                await _appRepository.CreateAsync(new AppLog((int)ELog.Error, "", ""));
+                return ResultHandler<Transaction>.Fail("Falha ao criar a Transação do usuário.");
+            }
+
+            return ResultHandler<Transaction>.Ok("Transação criado com sucesso.", transaction);
+        }
+
+        catch (Exception ex) when (ex is DomainException or EnumException)
+        {
+            var errorMsg = ex.InnerException?.Message ?? ex.Message ?? "Erro de validação.";
+            await _appRepository.CreateAsync(new AppLog((int)ELog.Validation, "Create", $"Erro na validação ao criar a transaction do usuário: -> {errorMsg}"));
+            return ResultHandler<Transaction>.Fail(errorMsg);
+        }
+
+        catch (Exception e)
+        {
+            var errorMsg = e.InnerException?.Message ?? e.Message ?? "Erro Desconhecido";
+            await _appRepository.CreateAsync(new AppLog((int)ELog.Error, "Create", $"Ocorreu um erro na criar a transaction do usuário: {errorMsg}"));
+            return ResultHandler<Transaction>.Fail($"Ocorreu um erro interno no servidor. Tente novamente mais tarde.");
+        }
+    }
+}
