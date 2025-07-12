5c8425c2f3da158775313fa1deaa1779613c2f2c
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-07-12 11:54:32 -0300

    feat: configurado o DTO para a rota de Transaction na API, Closes #54

---
+++ Finlyze.Application/DTO/TransactionDto/CreateTransactionDto.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Finlyze.Application.Dto;
+
+public class CreateTransactionDto
+{
+    [Required(ErrorMessage = "O campo de Title é obrigatório.")]
+    [MaxLength(100, ErrorMessage = "O campo de Title deve ter no máximo 100 caracteres.")]
+    public string Title { get; set; }
+
+    [MaxLength(300, ErrorMessage = "A Description deve ter no máximo 300 caracteres.")]
+    public string? Description { get; set; }
+
+    [Required(ErrorMessage = "O campo de Amount é obrigatório.")]
+    public decimal Amount { get; set; }
+
+    [Required(ErrorMessage = "O campo de Type é obrigatório.")]
+    public int Type { get; set; }
+
+    [DataType(DataType.DateTime, ErrorMessage = "O campo CreateAt é inválido.")]
+    public DateTime CreateAt { get; set; }
+}
