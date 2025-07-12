396e4fe26d6561c627381144c5f4f69bff09614c
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-07-12 12:04:31 -0300

    fix: ajuste nos parametro que estava faltando no construtor da entidade

--- Finlyze.Application/Common/EntityMap/MapRawToTransaction.cs
+++ Finlyze.Application/Common/EntityMap/MapRawToTransaction.cs
@@ -4,6 +4,6 @@ namespace Finlyze.Application.Entity.Raw.Convert;
 
 public static class MapRawToTransaction
 {
-    public static Transaction ToSingleTransaction(this TransactionRaw transaction_raw) => new Transaction(transaction_raw.Id, transaction_raw.TransactionTitle, transaction_raw.TransactionDescription, transaction_raw.Amount, transaction_raw.TypeTransaction, transaction_raw.TransactionCreateAt, transaction_raw.TransactionUpdateAt);
+    public static Transaction ToSingleTransaction(this TransactionRaw transaction_raw) => new Transaction(transaction_raw.Id, transaction_raw.TransactionTitle, transaction_raw.TransactionDescription, transaction_raw.Amount, transaction_raw.TypeTransaction, transaction_raw.TransactionCreateAt, transaction_raw.TransactionUpdateAt, transaction_raw.UserAccountId);
     public static IEnumerable<Transaction> ToEnumerableTransaction(this IEnumerable<TransactionRaw> raws) => raws.Select(raw => raw.ToSingleTransaction());
 }
