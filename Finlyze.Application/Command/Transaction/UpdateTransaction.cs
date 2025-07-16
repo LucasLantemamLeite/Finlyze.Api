09d403b30c092e8c8351d2e7646c0688352a2361
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-07-16 14:29:10 -0300

    feat: adicionado a propriedade Id, e removido o Update, o servidor irá gerar essa data

--- Finlyze.Application/Command/Transaction/UpdateTransaction.cs
+++ Finlyze.Application/Command/Transaction/UpdateTransaction.cs
@@ -2,20 +2,20 @@ namespace Finlyze.Application.Abstract.Interface.Command;
 
 public class UpdateTransactionCommand
 {
+    public int Id { get; set; }
     public string TransactionTitle { get; set; }
     public string? TransactionDescription { get; set; }
     public decimal Amount { get; set; }
     public int TypeTransaction { get; set; }
     public DateTime TransactionCreateAt { get; set; }
-    public DateTime TransactionUpdateAt { get; set; }
 
-    public UpdateTransactionCommand(string title, string description, decimal amount, int type, DateTime create, DateTime update)
+    public UpdateTransactionCommand(int id, string title, string description, decimal amount, int type, DateTime create)
     {
+        Id = id;
         TransactionTitle = title;
         TransactionDescription = description;
         Amount = amount;
         TypeTransaction = type;
         TransactionCreateAt = create;
-        TransactionUpdateAt = update;
     }
 }
