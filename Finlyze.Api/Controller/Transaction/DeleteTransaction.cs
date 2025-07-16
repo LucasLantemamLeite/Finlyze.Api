c4d175b5353a68b5df604f10ac8fc98485699bd7
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-07-15 22:17:41 -0300

    feat: criado a rota para deletar uma transaction, Closes #58

---
+++ Finlyze.Api/Controller/Transaction/DeleteTransaction.cs
@@ -0,0 +1,49 @@
+using System.Security.Claims;
+using Finlyze.Application.Abstract.Interface.Command;
+using Finlyze.Application.Abstract.Interface.Handler;
+using Finlyze.Application.Dto;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Finlyze.Api.Controller.Transactions;
+
+[ApiController]
+[Route("api/v1")]
+[Tags("Transactions")]
+public class DeleteTrasactionController : ControllerBase
+{
+    private readonly IDeleteTransactionHandler _handler;
+
+    public DeleteTrasactionController(IDeleteTransactionHandler handler) => _handler = handler;
+
+    [Authorize]
+    [HttpDelete("transaction")]
+    public async Task<IActionResult> CreateAsync([FromBody] DeleteTransactionDto tran_dto)
+    {
+        try
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            if (userId is null)
+                return Unauthorized();
+
+            var command = new DeleteTransactionCommand(tran_dto.Id);
+            var result = await _handler.Handle(command);
+
+            if (!result.Success)
+            {
+                return BadRequest(new { result.Message });
+            }
+
+            return Ok(new { Message = "Transaction deletada com sucesso." });
+        }
+
+        catch
+        {
+            return StatusCode(500, new { Message = $"Erro interno do servidor. Tente novamente mais tarde." });
+        }
+    }
+}
