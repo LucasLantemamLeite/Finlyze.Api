4f2bd1fd16428e2e822720a8628d12adf4a20a40
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-07-17 11:22:09 -0300

    chore: alteração no nome do arquivo e classe

--- Finlyze.Application/Common/MapperRaw/MapRawToSystemLog.cs
+++ Finlyze.Application/Common/MapperRaw/MapRawToSystemLog.cs
@@ -2,7 +2,7 @@ using Finlyze.Domain.Entity;
 
 namespace Finlyze.Application.Entity.Raw.Convert;
 
-public static class MapRawToAppLog
+public static class MapRawToSystemLog
 {
     public static SystemLog ToSingleAppLog(this AppLogRaw app_raw) => new SystemLog(app_raw.Id, app_raw.LogType, app_raw.Title, app_raw.Description, app_raw.CreateAt);
     public static IEnumerable<SystemLog> ToEnumerableAppLog(this IEnumerable<AppLogRaw> logs) => logs.Select(log => log.ToSingleAppLog());
