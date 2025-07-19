5ad775697b99a24c73e84cfeb0f1c672831e8b57
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-07-18 22:54:01 -0300

    refactor: alterado de Fact para Theory para testar vários valores diferentes possíveis

--- Finlyze.Test/Tests/QueriesTests/FakeUserAccountQueryTest.cs
+++ Finlyze.Test/Tests/QueriesTests/FakeUserAccountQueryTest.cs
@@ -40,66 +40,70 @@ public class FakeUserAccountQueryTest
         _userQuery = new FakeUserAccountQuery(_users);
     }
 
-    [Fact]
-    public async Task Dado_Um_Id_Existente_Trás_Entidade_Do_Banco()
+    [Theory]
+    [InlineData("11111111-1111-1111-1111-111111111111")]
+    [InlineData("22222222-2222-2222-2222-222222222222")]
+    public async Task Dado_Um_Id_Existente_Trás_Entidade_Do_Banco(Guid id)
     {
-        var id = Guid.Parse("22222222-2222-2222-2222-222222222222");
-
         var result = await _userQuery.GetByIdAsync(id);
 
         Assert.NotNull(result);
         Assert.True(_users.Any(x => x.Id == id));
     }
 
-    [Fact]
-    public async void Dado_Um_Id_Inexistente_Não_Trás_Do_Banco()
+    [Theory]
+    [InlineData("33333333-3333-3333-3333-333333333333")]
+    [InlineData("44444444-4444-4444-4444-444444444444")]
+    [InlineData("55555555-5555-5555-5555-555555555555")]
+    [InlineData("99999999-9999-9999-9999-999999999999")]
+    public async void Dado_Um_Id_Inexistente_Não_Trás_Do_Banco(Guid id)
     {
-        var id = Guid.Parse("22222222-2222-2222-2222-222222222223");
-
         var result = await _userQuery.GetByIdAsync(id);
 
         Assert.Null(result);
         Assert.True(!_users.Any(x => x.Id == id));
     }
 
-    [Fact]
-    public async void Dado_Um_Email_Existente_Trás_Do_Banco()
+    [Theory]
+    [InlineData("maria.silva@finlyze.com")]
+    [InlineData("lucas.leite@finlyze.com")]
+    public async void Dado_Um_Email_Existente_Trás_Do_Banco(string email)
     {
-        var email = "maria.silva@finlyze.com";
-
         var result = await _userQuery.GetByEmailAsync(email);
 
         Assert.NotNull(result);
         Assert.True(_users.Any(x => x.Email.Value == email));
     }
 
-    [Fact]
-    public async void Dado_Um_Email_Inexistente_Não_Trás_Do_Banco()
+    [Theory]
+    [InlineData("emailinexistente1@gmail.com")]
+    [InlineData("fulanocabramacho@gmail.com")]
+    [InlineData("nãoexisteesseemail@gmail.com")]
+    public async void Dado_Um_Email_Inexistente_Não_Trás_Do_Banco(string email)
     {
-        var email = "emailinexistente@gmail.com";
-
         var result = await _userQuery.GetByEmailAsync(email);
 
         Assert.Null(result);
         Assert.True(!_users.Any(x => x.Email.Value == email));
     }
 
-    [Fact]
-    public async void Dado_Um_PhoneNumber_Existente_Trás_Do_Banco()
+    [Theory]
+    [InlineData("+5541999991234")]
+    [InlineData("+5511988884321")]
+    public async void Dado_Um_PhoneNumber_Existente_Trás_Do_Banco(string phone)
     {
-        var phone = "+5541999991234";
-
         var result = await _userQuery.GetByPhoneNumberAsync(phone);
 
         Assert.NotNull(result);
         Assert.True(_users.Any(x => x.PhoneNumber.Value == phone));
     }
 
-    [Fact]
-    public async void Dado_Um_PhoneNumber_Inexistente_Não_Trás_Do_Banco()
+    [Theory]
+    [InlineData("+5562969445563")]
+    [InlineData("+5593981777179")]
+    [InlineData("+5535987832132")]
+    public async void Dado_Um_PhoneNumber_Inexistente_Não_Trás_Do_Banco(string phone)
     {
-        var phone = "+5562969445563";
-
         var result = await _userQuery.GetByPhoneNumberAsync(phone);
 
         Assert.Null(result);
