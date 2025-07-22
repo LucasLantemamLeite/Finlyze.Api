7eb98cc581b4f5272db5d2fb0e3f5152d9eaa60b
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-07-22 16:19:22 -0300

    feat: criado o handler para atualizar a conta do usuário

---
+++ Finlyze.Test/Fakes/UserAccountFakes/FakeHandlers/FakeUpdateUserAccountHandler.cs
@@ -0,0 +1,47 @@
+using Finlyze.Application.Abstract.Interface;
+using Finlyze.Application.Abstract.Interface.Command;
+using Finlyze.Application.Abstract.Interface.Handler.Result;
+using Finlyze.Application.Authentication.Hasher;
+using Finlyze.Domain.Entity;
+using Finlyze.Test.Abstract.Interfaces.Queries.Fakes.UserAccountFake;
+using Finlyze.Test.Abstract.Interfaces.Repositories.Fakes.UserAccountFake;
+
+namespace Finlyze.Test.Abstract.Interfaces.Handlers.Fakes.UserAccountFake;
+
+public class FakeUpdateUserAccountHandler : IUpdateUserAccountHandler
+{
+    private readonly List<UserAccount> _users = new();
+    private readonly FakeUserAccountQuery _userQuery;
+    private readonly FakeUserAccountRepository _userRepository;
+
+    public FakeUpdateUserAccountHandler(List<UserAccount> users)
+    {
+        _users = users;
+        _userQuery = new FakeUserAccountQuery(_users);
+        _userRepository = new FakeUserAccountRepository(_users);
+    }
+
+    public async Task<ResultHandler<UserAccount>> Handle(UpdateUserAccountCommand command)
+    {
+        var existingUser = await _userQuery.GetByIdAsync(command.Id);
+
+        if (existingUser is null)
+            return ResultHandler<UserAccount>.Fail("Conta do usuário com esse Id não encontrado.");
+
+        if (!existingUser.Password.Value.VerifyHash(command.ConfirmPassword))
+            return ResultHandler<UserAccount>.Fail("Senha incorreta.");
+
+        existingUser.ChangeName(command.Name);
+        existingUser.ChangeEmail(command.Email);
+        existingUser.ChangePassword(command.Password);
+        existingUser.ChangePhoneNumber(command.PhoneNumber);
+        existingUser.ChangeBirthDate(command.BirthDate);
+
+        var rows = await _userRepository.UpdateAsync(existingUser);
+
+        if (rows == 0)
+            return ResultHandler<UserAccount>.Fail("Erro ao atualizar a conta do usuário.");
+
+        return ResultHandler<UserAccount>.Ok("Conta do usuário atualizado com sucesso.", existingUser);
+    }
+}
