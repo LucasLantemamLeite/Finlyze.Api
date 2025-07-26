521cca537ca44eecf264c12e52a0a4848346b62d
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-07-26 20:00:55 -0300

    feat: cria o Fake handler para criar conta de usuários admin

---
+++ Finlyze.Test/Fake/UserAccount/Handler/FakeCreateAdminUserAccountHandler.cs
@@ -0,0 +1,44 @@
+using Finlyze.Application.Abstracts.Interfaces.Commands;
+using Finlyze.Application.Abstracts.Interfaces.Handlers;
+using Finlyze.Application.Abstracts.Interfaces.Handlers.Result;
+using Finlyze.Domain.Entities.UserAccountEntity;
+using Finlyze.Domain.ValueObjects.Enums;
+using Finlyze.Test.Abstract.Interfaces.Queries.Fakes.UserAccountFakes;
+using Finlyze.Test.Abstract.Interfaces.Repositories.Fakes.UserAccountFakes;
+
+namespace Finlyze.Test.Abstract.Interfaces.Handlers.Fakes.UserAccountFakes;
+
+public class FakeCreateAdminUserAccountHandler : ICreateAdminUserAccountHandler
+{
+    private readonly List<UserAccount> _users = new();
+    private readonly FakeUserAccountQuery _userQuery;
+    private readonly FakeUserAccountRepository _userRepository;
+
+    public FakeCreateAdminUserAccountHandler(List<UserAccount> users)
+    {
+        _users = users;
+        _userQuery = new FakeUserAccountQuery(users);
+        _userRepository = new FakeUserAccountRepository(users);
+    }
+
+    public async Task<ResultHandler<UserAccount>> Handle(CreateUserAccountCommand command)
+    {
+        var existingEmail = await _userQuery.GetByEmailAsync(command.Email);
+
+        if (existingEmail is not null)
+            return ResultHandler<UserAccount>.Fail("Email já está sendo usado.");
+
+        var existingPhone = await _userQuery.GetByPhoneNumberAsync(command.PhoneNumber);
+
+        if (existingPhone is not null)
+            return ResultHandler<UserAccount>.Fail("Número de telefone já está sendo usado.");
+
+        var userAccount = new UserAccount(command.Name, command.Email, command.Password, command.PhoneNumber, command.BirthDate, true, (int)(ERole.User | ERole.Admin));
+        var rows = await _userRepository.CreateAsync(userAccount);
+
+        if (rows == 0)
+            return ResultHandler<UserAccount>.Fail("Erro ao criar conta do usuário.");
+
+        return ResultHandler<UserAccount>.Ok("Conta do usuário admin criado com sucesso.", userAccount);
+    }
+}
